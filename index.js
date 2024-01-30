08595ebce2c94547669fbcc98f23fc7c2d819e2f
Anirudh Hada <anirudhhada5032@gmail.com>
2024-01-30 18:47:54 +0530

    added js file

---
+++ index.js
@@ -0,0 +1,31 @@
+// Q1
+let a = "Anirudh Hada"
+let b = 5
+console.log(a + " " + b)
+
+// Q2
+console.log(typeof(a))
+
+// Q3
+const obj = {
+  name : "Anirudh Hada",
+  age : 23
+} 
+console.log(obj)
+// obj = "Anil"; // It will throw an error
+
+// Q4
+obj["friend"] = "Shubham"
+obj["name"] = "Kartik"
+console.log(obj)
+
+// Q5
+const dict = {
+  imponderabilia : "things that cannot be precisely determined, measured, or evaluated.",
+  appreciate : "recognize the full worth of.",
+  jannock : "honest; fair; straightforward.",
+  commodious : "spacious and convenient; roomy.",
+  insouciant : "free from concern, worry, or anxiety; carefree; nonchalant."
+}
+
+console.log(dict)
